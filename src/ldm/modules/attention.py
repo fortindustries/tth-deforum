2967f260f1a5393fde6e312b8cf5380cb36ad0b9
Alberto Fortin <hello@albertofortin.me>
2023-04-05 15:14:46 +0100

    remove xf

--- src/ldm/modules/attention.py
+++ src/ldm/modules/attention.py
@@ -16,6 +16,7 @@ try:
 except:
     XFORMERS_IS_AVAILBLE = False
 
+XFORMERS_IS_AVAILBLE = False
 # CrossAttn precision handling
 import os
 _ATTN_PRECISION = os.environ.get("ATTN_PRECISION", "fp32")
